e7aced1bb37ec334bd76fc929bfe1cd41caf0b18
carbon73 <theo.matrod@gmail.com>
2023-10-03 10:32:33 +0200

    Mise en place de Expo Router

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { Text,View,StatusBar } from 'react-native';
+
+
+export default function Page() {
+  return (
+    <View>
+      <Text style ={{color : '#fff'}}>Bienvenue sur le campus du Bourget du Lac !</Text>
+      <StatusBar style="auto" />
+
+              <Text>Commençons notre aventure</Text>
+
+    </View>
+  );
+}
